4bbfaff65a7aa44d0adc345b58380ce13042b1a0
Maxim Ugnichenko <ugmaxie@gmail.com>
2017-02-14 18:00:59 +0200

    fix(app component): fixes after self CR

--- src/edit-profile/edit-profile.component.spec.ts
+++ src/edit-profile/edit-profile.component.spec.ts
@@ -1,32 +1,32 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
-import { ArtistProfileComponent } from './artist-profile.component';
+import { EditProfileComponent } from './edit-profile.component';
 
 describe('HomeComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        ArtistProfileComponent
+        EditProfileComponent
       ],
     });
     TestBed.compileComponents();
   });
 
   it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(ArtistProfileComponent);
+    let fixture = TestBed.createComponent(EditProfileComponent);
     let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(ArtistProfileComponent);
+    let fixture = TestBed.createComponent(EditProfileComponent);
     let app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app works!');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(ArtistProfileComponent);
+    let fixture = TestBed.createComponent(EditProfileComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('app works!');
