3fba07f03fed0278126ec5810ea7a016a0811ba4
d.litvinov <dmutro.litvinov@gmail.com>
2017-12-15 17:35:44 +0200

    feat(sort events): are ready for test

--- src/my-events/my-events.component.ts
+++ src/my-events/my-events.component.ts
@@ -15,9 +15,23 @@ export class MyEventsComponent {
   constructor (private searchService: SearchService) {
     const query: string = Config.objToQuery({findByBuyers: '59ad6bde3a367a002dc6c500'});
     this.searchService.getMyEvents(query).subscribe(res => {
-      this.shows = res;
+      this.shows = this.sortShows(res);
     }, err => {
       console.log('error: ', err);
     })
+  }
+
+  sortShows(shows: LaunchEvent[] ): LaunchEvent[]  {
+    return shows.sort((a: LaunchEvent, b: LaunchEvent) => {
+      if (a.live || b.live) {
+        return a.live ? -1 : 1;
+      }
+
+      if ((!a.live && !a.completed) || (!b.live && !b.completed) ) {
+        return !a.live && !a.completed ? -1 : 1
+      }
+
+      return 0;
+    });
   }
 }
