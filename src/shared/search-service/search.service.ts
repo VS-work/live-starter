66b7f40e682e9dfad6526a2e818846109f58ba58
DmitriyLitvinov <dmutro.litvinov@gmail.com>
2018-03-15 12:49:15 +0200

    enhancement(HttpClient): added catchError to all httpClient menthods

--- src/shared/search-service/search.service.ts
+++ src/shared/search-service/search.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import  'rxjs/add/operator/catch';
-import  'rxjs/add/observable/throw';
-import 'rxjs/add/operator/map';
+import { catchError } from 'rxjs/operators';
 
 import { Config } from '../../app.config';
 import { Show } from '../../event-launch/event-launch.model';
@@ -40,44 +38,68 @@ export class SearchService {
 
   getMusicStyles(): Observable<string[]> {
     return this.http.get(`${Config.api}/get-genres`)
-      .catch(err => Observable.of(err));
+      .pipe(catchError(err => {
+        console.error('something went wrong: ', err);
+        return Observable.throw(err.error)
+      }));
   }
 
   getGenres(): Observable<string[]> {
     return this.http.get(`${Config.api}/get-genres`)
-      .catch(err => Observable.of(err));
+      .pipe(catchError(err => {
+        console.error('something went wrong: ', err);
+        return Observable.throw(err.error)
+      }));
   }
 
   getLocations(): Observable<Location> {
     return this.http.get(`${Config.api}/get-locations`)
-      .catch(err => Observable.of(err));
+      .pipe(catchError(err => {
+        console.error('something went wrong: ', err);
+        return Observable.throw(err.error)
+      }));
   }
 
   getNonLiveEventsAmountData(): Observable<number> {
     return this.http.get(`${Config.api}/get-non-live-events-amount`)
-      .catch(err => Observable.of(err));
+      .pipe(catchError(err => {
+        console.error('something went wrong: ', err);
+        return Observable.throw(err.error)
+      }));
   }
 
   getEventsList(query: {[key: string]: any}): Observable<Show[]> {
     const newQuery = {...query, userId: this.userProfile ? this.userProfile._id : ''};
 
     return this.http.get(`${Config.api}/get-events-list-by-query?${Config.objToQuery(newQuery)}`)
-      .catch(err => Observable.of(err));
+      .pipe(catchError(err => {
+        console.error('something went wrong: ', err);
+        return Observable.throw(err.error)
+      }));
   }
 
   getUserFollowings(query: string): Observable<ShowInfo[]> {
     return this.http.get(`${Config.api}/get-user-followings?${query}`)
-      .catch(err => Observable.of(err));
+      .pipe(catchError(err => {
+        console.error('something went wrong: ', err);
+        return Observable.throw(err.error)
+      }));
   }
 
   getArtistsByQuery(query: QueryToFindArtists): Observable<ShowInfo[]> {
     const querySting = Config.objToQuery(query);
     return this.http.get(`${Config.api}/get-artists-by-query?${querySting}`)
-      .catch(err => Observable.throw(err));
+      .pipe(catchError(err => {
+        console.error('something went wrong: ', err);
+        return Observable.throw(err.error)
+      }));
   }
 
   getArtisAmount(): Observable<number> {
     return this.http.get(`${Config.api}/get-artists-amount`)
-      .catch(err => Observable.throw(err));
+      .pipe(catchError(err => {
+        console.error('something went wrong: ', err);
+        return Observable.throw(err.error)
+      }));
   }
 }
