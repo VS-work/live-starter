5e3984c141c3211014ea10049df70f33098df607
DmitriyLitvinov <dmutro.litvinov@gmail.com>
2018-04-24 17:00:10 +0300

    feat(how it works): how it works page is ready for test   - added responsive markup;   - adde dynamic data for page;

--- src/how-it-works/how-it-works.module.ts
+++ src/how-it-works/how-it-works.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+
 import { HowItWorksComponent } from './how-it-works.component';
 import { routing } from '../modules/how-it-works.routing';
+import { HowItWorksService } from './how-it-works.service';
 
 @NgModule({
-  imports: [CommonModule, routing],
+  imports: [
+    CommonModule,
+    FormsModule,
+    routing,
+    ButtonsModule.forRoot()
+  ],
+  providers: [HowItWorksService],
   declarations: [HowItWorksComponent]
 })
 
