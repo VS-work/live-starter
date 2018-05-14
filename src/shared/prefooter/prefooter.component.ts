87a7c4c740dcb65cd1e339410e0ba208ce7fbfe9
DmitriyLitvinov <dmutro.litvinov@gmail.com>
2018-05-14 17:58:21 +0300

    feat(prefooter): #101 is ready for test   - added router link to `Learn more` button

--- src/shared/prefooter/prefooter.component.ts
+++ src/shared/prefooter/prefooter.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { AuthService } from '../../auth0/auth.service';
+import { RouterLinks } from '../../enums/router-links.emum';
 
 @Component({
   selector: 'app-prefooter',
@@ -9,11 +11,15 @@ import { AuthService } from '../../auth0/auth.service';
 })
 
 export class PrefooterComponent {
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   signUp(): void {
     this.authService.showSignUpModal();
   };
+
+  learnMore(): void {
+    this.router.navigate([RouterLinks.HowItWorks]);
+  }
 }
 
